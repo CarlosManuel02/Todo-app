544c68912d65b8656db3a77372e72d364606f4cd
CarlosManuel02 <94581164+CarlosManuel02@users.noreply.github.com>
2022-10-24 19:59:14 -0700

    Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoInterface} from "./todo/interfaces/todo.interface";
+import {TodoService} from "./todo/services/todo.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo-app';
+    title = 'TodoInterface-app';
+
+    constructor(private todoService: TodoService) {
+    }
+
+
+    get todos(): TodoInterface[] {
+        return this.todoService.allTodos;
+    }
+
+    addTodo(todo: string): void {
+        this.todoService.addTodo(todo);
+    }
+
+    clearCompleted(): void {
+        this.todoService.clearCompleted();
+    }
+
+    toggleDone = (todo: TodoInterface) => this.todoService.toggleDone(todo);
+
 }
---
+++ src/app/todo/interfaces/todo.interface.ts
@@ -0,0 +1,4 @@
+export interface TodoInterface {
+    description: string;
+    done: boolean;
+}
